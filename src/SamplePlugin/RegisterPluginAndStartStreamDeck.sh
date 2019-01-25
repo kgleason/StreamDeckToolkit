41d6b61c80c6b9d71266b13ac9e0c577dbf23bec
Kirk <kgleason@gmail.com>
2019-01-25 11:08:05 -0500

    Wrapped the paths in quotes
    Wrapped all paths in quotes to deal with shell interpolation issues.
    Placed the PLUGIN_DIR variable after the definition of ${pluginName}.
    Fixed a variable naming issue in PLUGIN_DIR.
    simplified the rm -f line.

--- src/SamplePlugin/RegisterPluginAndStartStreamDeck.sh
+++ src/SamplePlugin/RegisterPluginAndStartStreamDeck.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#Define Plugin Directory
-PLUGIN_DIR="~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/${pluginName.sdPlugin}"
 #Define suffix
 SUFFIX=".action"
 
@@ -16,17 +14,20 @@ uuid=$(sed -n 's/.*"UUID": "\(.*\)"/\1/p' manifest.json)
 
 pluginName=$(echo "${uuid}" | tr -d \" | sed -e "s/${SUFFIX}$//")
 
+#Define Plugin Directory
+PLUGIN_DIR="~/Library/Application\ Support/com.elgato.StreamDeck/Plugins/${pluginName}.sdPlugin"
+
 #Notify user of Action.
 echo "Installing the ${pluginName} plugin..."
 
 #On first run the file does not exist, we check if it exists.
-if [ -d ${PLUGIN_DIR} ]; then rm -fr ${PLUGIN_DIR}; fi
+[[ -d "${PLUGIN_DIR}" ]] && rm -fr "${PLUGIN_DIR}"
 
 #Recreate directory for plugin.
-mkdir -p ${PLUGIN_DIR}
+mkdir -p "${PLUGIN_DIR}"
 
 #Move our build into folder.
-cp bin/Debug/netcoreapp2.2/* ${PLUGIN_DIR}
+cp bin/Debug/netcoreapp2.2/* "${PLUGIN_DIR}"
 
 #Notify user of Action.
 echo "Done installing ${pluginName}."
